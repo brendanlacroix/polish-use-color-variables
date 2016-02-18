6555bfdd464666e3b151dea51062a119fdfc99d2
Brendan LaCroix <brendan@animoto.com>
2016-02-18 15:29:19 -0500

    tests: Add another test case to confirm rgba function works in shorthand properties

--- tests/plugin.js
+++ tests/plugin.js
@@ -26,12 +26,13 @@ define(function (require) {
 
         errors = plugin.test(gonzales.parse(stylesheet.toString('utf8'), { syntax : 'scss' }));
 
-        assert.strictEqual(errors.length, 5);
+        assert.strictEqual(errors.length, 6);
         assert.equal(errors[0].node.toString().trim(), '#707020');
         assert.equal(errors[1].node.toString().trim(), 'palegoldenrod');
         assert.equal(errors[2].node.toString().trim(), '1px solid #707020');
         assert.equal(errors[3].node.toString().trim(), 'rgba(0, 0, 0, 0.4)');
         assert.equal(errors[4].node.toString().trim(), 'rgb(255, 255, 255, 0.5)');
+        assert.equal(errors[5].node.toString().trim(), 'rgba(1, 1, 1, 0.5)');
       }));
     }
   });
